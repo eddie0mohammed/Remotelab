beac934b30e9e7311a7b155e02e91ae7bf1135fd
Mohammad Edun <medun@e1z3r4p24.42.us.org>
2018-12-01 19:58:36 -0800

    12/01

---
+++ exam_prep/level4/ft_itoa/ft_itoa1.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char	*ft_itoa(int num)
+{
+	int i;
+	long n;
+	int base = 10;
+	int sign;
+	char *str;
+
+	n = (num < 0) ? -(long)num : num;
+	sign = (num < 0 && base == 10) ? -1 : 1;
+	i = (sign == -1) ? 2 : 1;
+	while ((n /= base) >= 1)
+		i++;
+	if (!(str = (char*)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	str[i] = '\0';
+	n = (num < 0) ? -(long)num : num;
+	while (i-- +sign)
+	{
+		str[i] = (n % base < 10) ? n % base + '0' : n % base + 'A' - 10;
+		n /= base;
+	}
+	(i == 0) ? str[i] = '-' : 0;
+	return (str);
+
+}
+
+int main(void)
+{
+	printf("%s\n", ft_itoa(-12345));
+	return 0;
+}
---
+++ exam_prep/level4/ft_list_remove_if/ft_list_remove_if1.c
@@ -0,0 +1,26 @@
+#include "ft_list.h"
+
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	t_list	*current;
+	t_list 	*temp;
+
+	while (*begin_list && !cmp((*begin_list)->data, data_ref))
+	{
+		temp = *begin_list;
+		*begin_list = (*begin_list)->next;
+		free(temp);
+	}
+	current = *begin_list;
+	while (current & current->next)
+	{
+		if (!cmp(current->next->data, data_ref))
+		{
+			temp = current->next;
+			current->next = temp->next;
+			free(temp);
+		}
+		if (current->next)
+			current = current->next;
+	}
+}
